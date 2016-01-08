3d5e1fa42b82af45efb86d3ecaed8d4751481f92
Emil Kattainen <emilkattainen@gmail.com>
2016-01-08 05:05:59 +0200

    Added match class

---
+++ EEnginev2/src/Main/Match.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Main;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Lime
+ */
+public class Match {
+    private ArrayList<ClientServer> clients;
+    private Game game;
+    public Match() {
+        game = new Game();
+        
+    }
+    public void update() {
+        
+    }
+    
+}
